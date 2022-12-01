1946e413d00a2b9efc3d20bd942b8e2cdb1412a6
nguyendinhphong123 <deepsea221297@gmail.com>
2022-12-01 08:34:06 +0700

    bai tap

---
+++ M1/006/index.js
@@ -0,0 +1,8 @@
+function do_something() {
+    alert("Hello word");
+}
+var demo = document.getElementById("demno");
+demo.addEventListener("click",clickFunction);
+function clickFunction() {
+    alert("Xin chào");
+}
